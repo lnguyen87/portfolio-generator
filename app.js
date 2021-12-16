6b5968fa6480a4255fa333b67111764656612a49
lnguyen87 <luc_nguyen@outlook.com>
2021-12-15 21:40:21 -0700

    added function to generate webpage with node.js

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
+const profileDataArgs = process.argv.slice(2);
 
-    console.log('================');
+const [name, github] = profileDataArgs;
 
-    // Is the same as this
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem)
-    });
-  };
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error (err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
